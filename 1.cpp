5814ba2dce0ea11f36243e0455ae5eeb0e3e1f3e
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-10-13 16:45:51 +0000

    done

--- 1.cpp
+++ 1.cpp
@@ -1,48 +1,11 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
-double calculateAbsoluteError(double trueValue, double approxValue) {
-    return abs(trueValue - approxValue);
-}
-
-double calculateRelativeError(double trueValue, double approxValue) {
-    return abs((trueValue - approxValue) / trueValue);
-}
-
-double calculatePercentageError(double trueValue, double approxValue) {
-    return calculateRelativeError(trueValue, approxValue) * 100.0;
-}
-
-int main() {
-    double trueValue, approxRoundoff, approxTruncation;
-
-    cout << "Enter the true value: ";
-    cin >> trueValue;
-
-    cout << "Enter the approximated value using roundoff: ";
-    cin >> approxRoundoff;
-
-    cout << "Enter the approximated value using truncation: ";
-    cin >> approxTruncation;
-    
-    double absoluteErrorRoundoff = calculateAbsoluteError(trueValue, approxRoundoff);
-    double relativeErrorRoundoff = calculateRelativeError(trueValue, approxRoundoff);
-    double percentageErrorRoundoff = calculatePercentageError(trueValue, approxRoundoff);
-    
-    double absoluteErrorTruncation = calculateAbsoluteError(trueValue, approxTruncation);
-    double relativeErrorTruncation = calculateRelativeError(trueValue, approxTruncation);
-    double percentageErrorTruncation = calculatePercentageError(trueValue, approxTruncation);
-    
-    cout << "For Roundoff:" << endl;
-    cout << "Absolute Error: " << absoluteErrorRoundoff << endl;
-    cout << "Relative Error: " << relativeErrorRoundoff << endl;
-    cout << "Percentage Error: " << percentageErrorRoundoff << "%" << endl;
-    
-    cout << "For Truncation:" << endl;
-    cout << "Absolute Error: " << absoluteErrorTruncation << endl;
-    cout << "Relative Error: " << relativeErrorTruncation << endl;
-    cout << "Percentage Error: " << percentageErrorTruncation << "%" << endl;
-    
+int main()
+{
+    double true_value,approx_value;
+    cin>>true_value>>approx_value;
+    cout<< "absolute error = "<< true_value-approx_value<<endl;
+    cout<< "relative error = "<< (true_value-approx_value)/true_value<<endl;
+    cout<< "percentage error = "<< ((true_value-approx_value)/true_value)*100;
     return 0;
 }
--- 2.cpp
+++ 2.cpp
@@ -7,14 +7,11 @@ int main(){
     cout<<"Enter values of x0 and x1: "<<endl;
     cin>>x0>>x1;
     while(x1-x0>0.0001){
-        x2=(x0+x1)/2;
-        cout<<"value of x2: "<<x2<<endl;
-        if(f(x2)<0){
-            x0=x2;
-        }
-        else{
-            x1=x2;
-        }
+        x2 = (x1+x0)/2;
+        if(f(x2)==0) break;
+        else if(f(x2)<0) x0 = x2;
+        else x1 = x2;
     }
+    cout<<"the root is = "<<x2;
     return 0;
 }
